1b0df2bcccb09288ad720366d5060106eddff9c4
helloWorld141 <nghia002@outlook.com>
2020-02-12 00:19:29 +0800

    config travis-ci and unittest

--- trading/LiquidityProvider.py
+++ trading/LiquidityProvider.py
@@ -1,5 +1,5 @@
 class LiquidityProvider(object):
-    def __init__(self, lp_2_gateway):
+    def __init__(self, lp_2_gateway=None):
         """
             @param lp_2_gateway : type collections.deque : push message to gateway
         """        
