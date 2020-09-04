300fd2a827cbeab3314d42f38875679563fc1028
helloWorld141 <nghia002@outlook.com>
2020-09-04 14:37:32 +0800

    setup test

--- trading/LiquidityProvider.py
+++ trading/LiquidityProvider.py
@@ -1,23 +1,38 @@
 from random import seed
 from trading.model.dto import Order
+import random
+import uuid
+import copy
 
 class LiquidityProvider(object):
     def __init__(self, lp_2_gateway=None):
         """
             @param lp_2_gateway : type collections.deque : push message to gateway
-        """        
+        """
         self.lp_2_gateway = lp_2_gateway
-        self.orders = []
+        self.orders: List[Order] = []
         seed(25)
 
     def generateRandomOrder(self):
-        #TODO
-        pass
+        id = uuid.uuid1().int
+        price = random.randint(8, 12)
+        quantity = random.randint(1,10) * 100
+        side = random.choice(['buy','sell'])
+        action = random.choice(['modify', 'delete']) if self.lookupOrder(id) else 'new'
+
+        order = Order(id, price, quantity, side, action)
+        self.orders.append(order)
+
+        if not self.lp_2_gateway:
+            print('simulation mode')
+            return order
+        self.lp_2_gateway.append(copy.deepcopy(order))
+
+
 
     def lookupOrder(self, id: int):
-        #TODO
-        pass
+        order = filter(lambda o: o.id == id, self.orders)
+        return order
 
     def insertManualOrder(self, order: Order):
-        #TODO
         pass
